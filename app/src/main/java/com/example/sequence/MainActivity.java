a805a44f1b4c5eab590c828dc05e09631068e4db
Andriy <ak8022@bs.amalnet.k12.il>
2020-09-25 19:04:39 +0300

    Updates questions

--- app/src/main/java/com/example/sequence/MainActivity.java
+++ app/src/main/java/com/example/sequence/MainActivity.java
@@ -3,14 +3,42 @@ package com.example.sequence;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity
 {
+    ToggleButton typeToggleButton;
+    TextView firstItem, difference;
+
+    boolean type; //false=arithmetic  true=geometric
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        typeToggleButton=(ToggleButton)findViewById(R.id.typeToggleButton);
+        firstItem=(TextView)findViewById(R.id.firstItem);
+        difference=(TextView)findViewById(R.id.difference);
+
+        type=false;
+    }
+
+    public void changeTypeOfProgression(View view)
+    {
+        type=typeToggleButton.isChecked();
+        if (type)
+        {
+            firstItem.setText("b1=");
+            difference.setText("q=");
+        }
+        else
+        {
+            firstItem.setText("a1=");
+            difference.setText("d=");
+        }
     }
 }
